e82df9a2ab0cb52357f8dbeae32e7e754ed6e3dc
laboratorio <laboratorio@UPOAULA10703>
2025-04-14 07:51:06 -0500

    Agregar archivos de proyecto.

---
+++ DemoEstudiante/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoEstudiante.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
